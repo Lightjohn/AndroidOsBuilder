dbf5e96f4e12f9dd31c492a7b6f45f2f5c8b0c3d
Lightyjohn <johnleger26@gmail.com>
2019-09-06 20:50:47 +0100

    More var  + final build

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+if [[ ! -d vendor/${BUILD_NAME} ]]; then
+  echo "WARNING no folder for ${BUILD_NAME} found in vendor, did you extracted the proprietary blobs"
+fi
 repo init -u https://github.com/LineageOS/android.git -b lineage-16.0
 echo "SYNCING" `date +"%m-%d-%Y %T"`
 repo sync > sync.log
-echo "BUILDING" `date +"%m-%d-%Y %T"`
+echo "CONFIGURING" `date +"%m-%d-%Y %T"`
 source build/envsetup.sh
-breakfast bacon
+breakfast "${BUILD_NAME}"
+# TODO CCACHE + SIGNED
+echo "BUILDING" `date +"%m-%d-%Y %T"`
+croot
+brunch "${BUILD_NAME}"
+echo "DONE output: ${OUT}"
