27f5d6f3a2a4fdfa9064cd1f95dd600679e1eec8
Lightjohn <johnleger26@gmail.com>
2020-12-13 20:37:00 +0000

    upgrade git blob repo

--- run_me_first_mondrianwifi.sh
+++ run_me_first_mondrianwifi.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Some Mondrianwifi proprietary stuff
-git clone https://github.com/Bisterne/vendor_samsung_mondrianwifi.git blobs_mondrianwifi
+git clone https://github.com/Valera1978/android_vendor_samsung_mondrianwifi.git blobs_mondrianwifi
 mkdir -p androidbuild/vendor/samsung/mondrianwifi
 cp -r blobs_mondrianwifi/* androidbuild/vendor/samsung/mondrianwifi
